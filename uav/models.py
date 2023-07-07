043b23e034b21c513e52ed62ac9b8fdc9dc8d954
busra <busraaceval@gmail.com>
2023-07-08 02:32:39 +0300

    CREATE: A fix model was created for the fields that are common to all models

--- uav/models.py
+++ uav/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# ---------------------------------
+# FixModel
+# ---------------------------------
+from django.contrib.auth.models import User
+
+class FixModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta: 
+        abstract = True
