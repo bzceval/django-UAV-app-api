af81475efc85547da4848879bab9bcb00f7873fd
busra <busraaceval@gmail.com>
2023-07-08 02:50:21 +0300

    CREATE: FixView for FixSerializer

--- uav/views.py
+++ uav/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import (
+    Uav, UavSerializer,
+    Reservation, ReservationSerializer,
+)
+
+# ---------------------------------
+# FixView
+# ---------------------------------
+class FixView(ModelViewSet):
+    pass
