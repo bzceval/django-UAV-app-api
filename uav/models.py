d8b2635ac943bf4789cd1c0430e9b4946a8129b7
busra <busraaceval@gmail.com>
2023-07-08 02:38:00 +0300

    ADD: uav model class

--- uav/models.py
+++ uav/models.py
@@ -12,3 +12,23 @@ class FixModel(models.Model):
 
     class Meta: 
         abstract = True
+
+# ---------------------------------
+# UAV
+# ---------------------------------
+from django.core.validators import MinValueValidator
+
+class Uav(FixModel): 
+    serial_number = models.CharField(max_length=16, unique=True)
+    brand = models.CharField(max_length=16)
+    model = models.CharField(max_length=16)
+    year = models.PositiveSmallIntegerField() 
+    rent_per_day = models.DecimalField(
+        max_digits = 8,
+        decimal_places = 2,
+        validators = [MinValueValidator(1)]
+    )
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.brand} {self.model} # {self.serial_number}'
