61c58030d0d29493fbdd622f11aeb0cf7317d55d
busra <busraaceval@gmail.com>
2023-07-07 22:57:17 +0300

    ADD: user view in user app views.py file

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import (User, UserSerializer)
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
+class UserView(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
