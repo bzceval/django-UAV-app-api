10098796284ce0d9f7337b4bed7f24e9d8a2c614
busra <busraaceval@gmail.com>
2023-07-08 02:55:14 +0300

    CREATE: draw routers for uav endpoint

--- uav/urls.py
+++ uav/urls.py
@@ -2,3 +2,13 @@ from django.urls import path, include
 
 # after '/api/' ->
 urlpatterns = [] 
+
+# -------------------------------
+# Routers
+# -------------------------------
+from rest_framework.routers import DefaultRouter
+from .views import UavView, ReservationView
+router = DefaultRouter()
+router.register('uav', UavView)
+router.register('reservation', ReservationView)
+urlpatterns += router.urls
