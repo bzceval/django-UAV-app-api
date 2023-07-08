d6cea4a5514354e1270e362a393a2372c2d4333b
busra <busraaceval@gmail.com>
2023-07-09 02:15:51 +0300

    ADD: login page design

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import login from "../assets/login.svg";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <div className="shadow-lg p-4">
+            <h1 className="fs-1 display-1 mb-5">Login</h1>
+            <div className="mb-4">
+              <p className="mb-1">Username:</p>
+              <input type="text" />
+            </div>
+            <div className="mb-4">
+              <p className="mb-1">Email:</p>
+              <input type="text" />
+            </div>
+            <div className="mb-4">
+              <p className="mb-1">Password:</p>
+              <input type="text" />
+            </div>
+            <div className="mb-4">
+              <div className="btn button-lr">Login</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 ">
+          <img src={login} alt="UVA Login" width={450} className="mt-5" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
