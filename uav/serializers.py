755aea2a440608025426364fa64f99bac1e61b31
busra <busraaceval@gmail.com>
2023-07-08 03:44:06 +0300

    UPDATE: Users are shown by name, not id

--- uav/serializers.py
+++ uav/serializers.py
@@ -10,7 +10,8 @@ from .models import (
 # FixSerializer
 # ---------------------------------
 class FixSerializer(serializers.ModelSerializer):
-    pass
+    user = serializers.StringRelatedField()
+    user_id = serializers.IntegerField(required=False, read_only=True)
 
 # ---------------------------------
 # UavSerializer
