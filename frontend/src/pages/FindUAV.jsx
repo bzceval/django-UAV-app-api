8a88d37efb42628d26d6cf96d441f496570a25e0
busra <busraaceval@gmail.com>
2023-07-09 01:36:27 +0300

    ADD: find uav page design

--- frontend/src/pages/FindUAV.jsx
+++ frontend/src/pages/FindUAV.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const FindUAV = () => {
   return (
-    <div>FindUAV</div>
-  )
-}
+    <div className="search-container pt-5 pb-5">
+      <div class="d-grid gap-2 col-6 mx-auto mt-3">
+        <input
+          type="search"
+          className="form-control text-center shadow-sm"
+          placeholder="Search UAV"
+        />
+      </div>
+      <div class="d-grid gap-2 col-3 mx-auto mt-3">
+        <button className="button">Search</button>
+      </div>
+    </div>
+  );
+};
 
-export default FindUAV
+export default FindUAV;
