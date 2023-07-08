18aa77d87b0020abf8de03ae05cff8cd48e3f433
busra <busraaceval@gmail.com>
2023-07-08 03:48:19 +0300

    UPDATE: Adding a name automatically, not by making a user select

--- uav/serializers.py
+++ uav/serializers.py
@@ -13,6 +13,10 @@ class FixSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
     user_id = serializers.IntegerField(required=False, read_only=True)
 
+    def create(self, validated_data):
+        validated_data['user_id'] = self.context['request'].user.id
+        return super().create(validated_data)
+
 # ---------------------------------
 # UavSerializer
 # ---------------------------------
