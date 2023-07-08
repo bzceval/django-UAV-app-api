c915a41f1cb3974811d8eab6e2521dd75ce7ede1
busra <busraaceval@gmail.com>
2023-07-09 00:58:06 +0300

    ADD: fe uav list design

--- frontend/src/pages/UAV.jsx
+++ frontend/src/pages/UAV.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const UAV = () => {
   return (
-    <div>UAV</div>
-  )
-}
+    <div className="container my-5">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div className="col">
+          <div className="card shadow-sm">
+            <svg
+              className="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: Thumbnail"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#55595c" />
+              <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                Thumbnail
+              </text>
+            </svg>
+            <div className="card-body">
+              <p className="card-text text-center">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div class="d-grid gap-2 col-6 mx-auto">
+                <button className="button">Reservation</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UAV
+export default UAV;
