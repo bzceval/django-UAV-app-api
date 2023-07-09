9c2535d127e85c37e54a8421918c8bb33c176821
busra <busraaceval@gmail.com>
2023-07-09 19:10:23 +0300

    UPDATE: development enviroment

--- backend/main/settings/__init__.py
+++ backend/main/settings/__init__.py
@@ -1,9 +1,9 @@
-# from .base import *
-# from .development import *
-from decouple import config
-ENVIRONMENT = config('ENV')
+from .base import *
+from .development import *
+# from decouple import config
+# ENVIRONMENT = config('ENV')
 
-if ENVIRONMENT=='development':
-    from .development import *
-else:
-    from .production import *
+# if ENVIRONMENT=='development':
+#     from .development import *
+# else:
+#     from .production import *
