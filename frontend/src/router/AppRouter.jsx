cfa5898d353b8305a1e9abf06700c65864e9d988
busra <busraaceval@gmail.com>
2023-07-09 12:59:57 +0300

    ADD: footer design

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import UAVDetail from "../pages/UAVDetail";
 import FindUAV from "../pages/FindUAV";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Footer from "../components/Footer";
 
 const AppRouter = () => {
   return (
@@ -18,6 +19,7 @@ const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
